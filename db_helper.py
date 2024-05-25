e67f8151df8559b1796a0a032141ec78734c826b
Vishnu <149374994+Vishnu2954@users.noreply.github.com>
2024-05-25 21:03:07 +0530

    Update db_helper.py

--- db_helper.py
+++ db_helper.py
@@ -1,29 +1,29 @@
-import csv
+import pandas as pd
 
 def get_precaution(disease):
     try:
-        with open('precaution_remedy.csv', 'r') as file:
-            csv_reader = csv.DictReader(file)
-            for row in csv_reader:
-                if row['Disease'] == disease:
-                    return row['Precaution']
-        return None 
+        df = pd.read_excel('precaution_remedy.xlsx')
+        row = df[df['Disease'] == disease]
+        if not row.empty:
+            return row.iloc[0]['Precaution']
+        else:
+            return None 
 
     except FileNotFoundError:
-        return "Error: CSV file not found"
+        return "Error: Excel file not found"
     except Exception as e:
         return f"Error: {e}"
 
 def get_remedy(disease):
     try:
-        with open('precaution_remedy.csv', 'r') as file:
-            csv_reader = csv.DictReader(file)
-            for row in csv_reader:
-                if row['Disease'] == disease:
-                    return row['Remedy']
-        return None  
-
+        df = pd.read_excel('precaution_remedy.xlsx')
+        row = df[df['Disease'] == disease]
+        if not row.empty:
+            return row.iloc[0]['Remedy']
+        else:
+            return None 
+            
     except FileNotFoundError:
-        return "Error: CSV file not found"
+        return "Error: Excel file not found"
     except Exception as e:
         return f"Error: {e}"
