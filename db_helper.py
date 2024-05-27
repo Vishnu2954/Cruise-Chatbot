fda0faf34b8ed6d4291cac347c27ed251039aabf
Vishnu <149374994+Vishnu2954@users.noreply.github.com>
2024-05-27 08:42:35 +0530

    Update db_helper.py

--- db_helper.py
+++ db_helper.py
@@ -27,3 +27,17 @@ def get_remedy(disease):
         return "Error: Excel file not found"
     except Exception as e:
         return f"Error: {e}"
+
+def get_causes(disease):
+    try:
+        df = pd.read_excel('precaution_remedy.xlsx')
+        row = df[df['Disease'] == disease]
+        if not row.empty:
+            return row.iloc[0]['Causes']
+        else:
+            return None 
+
+    except FileNotFoundError:
+            return "Error: Excel file not found"
+        except Exception as e:
+            return f"Error: {e}"
